182e57cf6920b29dba0258641153b5c07802e3cf
Hunter Peart <hman1148@outlook.com>
2025-03-07 11:07:48 -0700

    Initial commit

---
+++ back/src/main/java/com/ai_tutor/ai_tutor/AiInvestmentTutorApplication.java
@@ -0,0 +1,13 @@
+package com.ai_tutor.ai_tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInvestmentTutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInvestmentTutorApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/ai_tutor/ai_tutor/AiInvestmentTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_tutor.ai_tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInvestmentTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
